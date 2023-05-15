e61f8fe781ee219844e23df20e29c748addd1b91
JaeHyun-Lim-dev <bonny06@naver.com>
2023-05-15 18:07:48 +0900

    style: pages 폴더명 /pages -> /@pages로 변경

--- src/@pages/index.tsx
+++ src/@pages/index.tsx
@@ -3,3 +3,5 @@ export { default as MyPage } from "./MyPage";
 export { default as Invest } from "./Invest";
 export { default as Bank } from "./Bank";
 export { default as Login } from "./Login";
+export { default as Landing } from "./Landing";
+export { default as Register } from "./Register";
