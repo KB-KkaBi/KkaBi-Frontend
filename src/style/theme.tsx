c9c2cbfa67676e48bdb8d597ffd75ac4cabe4dfa
seojisoosoo <76681519+seojisoosoo@users.noreply.github.com>
2023-05-15 11:18:24 +0900

    Feat#1 common/settings (#6)
    * [ feat ] 초기 세팅
    
    * [ feat ] html 타이틀 변경
    
    ---------
    
    Co-authored-by: JaeHyun-Lim-dev <bonny06@naver.com>

---
+++ src/style/theme.tsx
@@ -0,0 +1,68 @@
+import { css } from "styled-components";
+
+const colors = {
+  main: "#FCAF16",
+  whiteYellow: "#FCF9EB",
+  green: "#BAEAAE",
+  blue: "#D0E9FF",
+  darkPink: "#F8C5C2",
+  darkYellow: "#FBC86A",
+  darkBlue: "#A7D5FF",
+};
+
+const fonts = {
+  title: css`
+    font-family: "KOTRA HOPE";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 8rem;
+    line-height: 4.8rem;
+  `,
+  text: css`
+    font-family: "KOTRA HOPE";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 4.6rem;
+    line-height: 5.4rem;
+  `,
+  button: css`
+    font-family: "KOTRA HOPE";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 4.5rem;
+    line-height: 5.2rem;
+  `,
+  cardTitle: css`
+    font-family: "KOTRA HOPE";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 6rem;
+    line-height: 4.8rem;
+  `,
+  quiz: css`
+    font-family: "KOTRA HOPE";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 6rem;
+    line-height: 7rem;
+  `,
+  log: css`
+    font-family: "KOTRA HOPE";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 3.5rem;
+    line-height: 4rem;
+  `,
+  won: css`
+    font-family: "yg-jalnan";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 2.5rem;
+    line-height: 2.6rem;
+  `,
+};
+
+export const theme = {
+  colors,
+  fonts,
+};
