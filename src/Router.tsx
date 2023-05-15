60ba6ad1add78ba0cf3e77d11d33d502749b569c
JaeHyun-Lim-dev <bonny06@naver.com>
2023-05-15 13:41:59 +0900

    feat: 로그인 페이지 navigate 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import * as P from "@/pages";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<P.Login />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<P.Login />} />
         <Route path="/home" element={<P.Home />} />
         <Route path="/mypage" element={<P.MyPage />} />
         <Route path="/bank" element={<P.Bank />} />
