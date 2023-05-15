66dfa70cf5ebf027fa3c51986c9e4db8bb4c1805
Jaehyun Lim <32118818+JaeHyun-Lim-dev@users.noreply.github.com>
2023-05-15 14:06:30 +0900

    feat: route 기본 설정 추가 (#8)
    * feat: router 초기 세팅
    * chore: 절대경로 설정을 위한 tsconfig 수정, viteconfig 추가
    * feat: route test용 빈 페이지 추가
    * feat: 로그인 페이지 navigate 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,9 +1,20 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import * as P from "@/pages";
 
-export default function Router() {
+const Router = () => {
   return (
     <BrowserRouter>
-      <Routes></Routes>
+      <Routes>
+        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<P.Login />} />
+        <Route path="/home" element={<P.Home />} />
+        <Route path="/mypage" element={<P.MyPage />} />
+        <Route path="/bank" element={<P.Bank />} />
+        <Route path="/invest" element={<P.Invest />} />
+      </Routes>
     </BrowserRouter>
   );
-}
+};
+
+export default Router;
