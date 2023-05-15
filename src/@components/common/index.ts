a3a0fc7989ea21e98a5f9b50d9fee36cc1969e0b
JaeHyun-Lim-dev <bonny06@naver.com>
2023-05-15 18:10:51 +0900

    feat: 공통 Input 추가

--- src/@components/common/index.ts
+++ src/@components/common/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from "./button/CommonButton";
 export { default as Modal } from "./modal/CommonModal";
+export { default as Input } from "./input/CommonInput";
---
+++ src/@components/common/input/CommonInput.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { TextFieldProps } from "@mui/material/TextField";
+import * as I from "./style";
+
+export default function Modal(props: TextFieldProps) {
+  const { ...args } = props;
+  return <I.CommonStyledInput {...args}></I.CommonStyledInput>;
+}
