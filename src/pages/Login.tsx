b9f8410dc8036c9ed15865e829f58be381bb6d07
JaeHyun-Lim-dev <bonny06@naver.com>
2023-05-15 15:57:50 +0900

    feat: 로그인 페이지에 공통 Button 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Button } from "@/@components";
 
 const Login = () => {
   return (
     <div>
       <h1>Login Page</h1>
+      <Button color="primary" onClick={() => console.log("click")}>
+        로그인
+      </Button>
     </div>
   );
 };
