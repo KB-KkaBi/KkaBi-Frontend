7787a6dd18ca6ad7aeaa4afa70c3743a5a45c00c
Seojisoo20191941 <amy0480@naver.com>
2023-05-16 14:13:58 +0900

    design: 보물, 은행 버튼 위치 수정

--- src/@pages/Home.tsx
+++ src/@pages/Home.tsx
@@ -5,8 +5,8 @@ import Background from "../assets/image/mainPageBackground.png";
 const Home = () => {
   return (
     <>
-      <MainPageTreasureIc />
-      <MainPageKkaBiBankIc />
+      <MainPageTreasureIcon />
+      <MainPageKkaBiBankIcon />
 
       <BackgroundImg src={Background} alt="배경화면" />
     </>
@@ -18,4 +18,15 @@ export default Home;
 const BackgroundImg = styled.img`
   position: absolute;
   z-index: -1;
+`;
+
+const MainPageTreasureIcon = styled(MainPageTreasureIc)`
+  position: absolute;
+  margin: 45.6rem 0 0 4rem;
+`;
+
+const MainPageKkaBiBankIcon = styled(MainPageKkaBiBankIc)`
+  position: absolute;
+  margin-top: 36.5rem;
+  right: 9.6rem;
 `;
