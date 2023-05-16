9c08155066984c17c19c7a90f02a8af0e8c99d15
Seojisoo20191941 <amy0480@naver.com>
2023-05-16 15:01:02 +0900

    feat: 캐릭터별로 다른 프로필 이미지 뜨도록 구현

--- src/@components/Home/MyInfo.tsx
+++ src/@components/Home/MyInfo.tsx
@@ -1,5 +1,13 @@
 import { styled } from "styled-components";
-import { HomeMyInfoIc } from "../../assets";
+import {
+  HomeMyInfoIc,
+  MiniProfileAgo,
+  MiniProfileBB,
+  MiniProfileKiKi,
+  MiniProfileKolly,
+  MiniProfileLamu,
+} from "../../assets";
+import { STAR_FRIENDS } from "@/core/starFriends";
 
 interface characterNameProps {
   characterName: String;
@@ -8,8 +16,27 @@ interface characterNameProps {
 const MyInfo = (props: characterNameProps) => {
   const { characterName } = props;
 
+  function miniProfile() {
+    switch (characterName) {
+      case STAR_FRIENDS.AGO:
+        return <MiniProfileAgo />;
+      case STAR_FRIENDS.BB:
+        return <MiniProfileBB />;
+      case STAR_FRIENDS.KIKI:
+        return <MiniProfileKiKi />;
+      case STAR_FRIENDS.KOLLY:
+        return <MiniProfileKolly />;
+      case STAR_FRIENDS.LAMU:
+        return <MiniProfileLamu />;
+
+      default:
+        return;
+    }
+  }
+
   return (
     <>
+      {miniProfile()}
       <HomeMyInfoIc />
     </>
   );
