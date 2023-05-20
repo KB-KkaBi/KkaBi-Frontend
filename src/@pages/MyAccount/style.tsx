550c412c10f740a0f48167a02e365598f3344ede
JaeHyun-Lim-FE <bonny06@naver.com>
2023-05-20 19:29:15 +0900

    feat: 내 계좌 페이지 view 추가

---
+++ src/@pages/MyAccount/style.tsx
@@ -0,0 +1,27 @@
+import { styled } from "styled-components";
+
+export const BottomButtonContainer = styled.div`
+  position: absolute;
+  bottom: 6rem;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  gap: 3.6rem;
+  width: 100%;
+`;
+
+export const TextContainer = styled.div`
+  margin: 9rem auto 0 auto;
+  display: block;
+  text-align: center;
+  width: fit-content;
+`;
+
+export const Guide = styled.p`
+  ${({ theme }) => theme.fonts.button}
+`;
+
+export const Won = styled.p`
+  ${({ theme }) => theme.fonts.won}
+  margin-bottom: 2.5rem;
+`;
