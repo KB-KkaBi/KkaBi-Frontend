5e37ae41b8951e93ad92270977aae68d67728f44
JaeHyun-Lim-FE <bonny06@naver.com>
2023-05-21 18:17:52 +0900

    feat: 예금/출금 페이지 view 추가

--- src/@pages/MyAccount/Withdraw.tsx
+++ src/@pages/MyAccount/Withdraw.tsx
@@ -1,14 +1,44 @@
 import { Button } from "@/@components";
-import { PaperLayout } from "@/@components/common/";
+import { Input, Modal, PaperLayout } from "@/@components/common/";
 import { useNavigate } from "react-router";
+import * as S from "./style";
+import { useState } from "react";
 
 const Withdraw = () => {
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const [amount, setAmount] = useState("");
+
+  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(e.target.value);
+  };
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => {
+    setOpen(false);
+    navigate("../", { replace: true });
+  };
 
   return (
     <PaperLayout handleClick={() => navigate(-1)}>
-      <h1>Withdraw Page</h1>
-      <Button onClick={() => console.debug("출금 확인")}>확인</Button>
+      <S.TransactionContent>
+        <S.Guide>얼마를 출금할까요?</S.Guide>
+        <Input
+          type="number"
+          onChange={handleAmountChange}
+          InputProps={{
+            endAdornment: <S.Won color={!!amount ? "#000" : "#aaaaaa"}>\</S.Won>,
+          }}></Input>
+        <S.Guide>출금 사유를 작성해주세요</S.Guide>
+        <Input placeholder="ex. 사탕 사먹기"></Input>
+        <Button onClick={handleOpen}>확인</Button>
+      </S.TransactionContent>
+      <Modal open={open}>
+        <>
+          <S.Guide>출금되었습니다</S.Guide>
+          <Button onClick={handleClose}>확인</Button>
+        </>
+      </Modal>
     </PaperLayout>
   );
 };
--- src/@pages/MyAccount/style.tsx
+++ src/@pages/MyAccount/style.tsx
@@ -19,9 +19,33 @@ export const TextContainer = styled.div`
 
 export const Guide = styled.p`
   ${({ theme }) => theme.fonts.button}
+  margin-bottom: 2.5rem;
 `;
 
-export const Won = styled.p`
+export const Won = styled.p<{ color?: string }>`
   ${({ theme }) => theme.fonts.won}
   margin-bottom: 2.5rem;
+  ${(props) => (props.color ? `color: ${props.color};` : "")}
+`;
+
+export const TransactionContent = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  padding-top: 20.5rem;
+  p {
+    margin-bottom: 0.2rem;
+  }
+  div + p {
+    margin-top: 4rem;
+  }
+  button {
+    margin-top: 9.5rem;
+  }
+  input[type="number"]::-webkit-inner-spin-button,
+  input[type="number"]::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
 `;
