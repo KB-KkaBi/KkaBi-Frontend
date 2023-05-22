a6c948924627e73ff9327d58a2fdc66aadce1867
Seojisoo20191941 <amy0480@naver.com>
2023-05-22 11:28:59 +0900

    design: 버튼 삽입

--- src/@pages/SelectAccount/BankMain.tsx
+++ src/@pages/SelectAccount/BankMain.tsx
@@ -1,7 +1,7 @@
 import { BackArrowIcon } from "@/@components/common/icon/Icons";
 import BankBackground from "@/assets/image/bankBankground.png";
 import { styled } from "styled-components";
-import { BankLayout } from "@/@components/common/";
+import { BankLayout, Button } from "@/@components/common/";
 
 const BankMain = () => {
   return (
@@ -11,6 +11,10 @@ const BankMain = () => {
           <p>KB 국민 은행에 오신 것을 환영합니다.</p>
           <p>무엇을 도와드릴까요?</p>
         </QuestionWrapper>
+        <ButtonWrapper>
+          <Button>계좌 개설</Button>
+          <Button>나의 계좌</Button>
+        </ButtonWrapper>
       </BankLayout>
     </>
   );
@@ -26,4 +30,13 @@ const QuestionWrapper = styled.h1`
   margin-top: 10rem;
 
   ${({ theme }) => theme.fonts.button}
+`;
+
+const ButtonWrapper = styled.section`
+  display: flex;
+  justify-content: space-evenly;
+
+  margin: 40rem 0 6rem 30rem;
+
+  width: 84rem;
 `;
