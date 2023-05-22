dd414672d1afebf77a97853cd23bc0ca6df87142
JaeHyun-Lim-dev <bonny06@naver.com>
2023-05-22 15:11:26 +0900

    feat: 거래내역 조회 초기 뷰 작업

---
+++ src/@pages/MyAccount/accountLog/AccountLog.tsx
@@ -0,0 +1,46 @@
+import { TransactionLogLayout } from "@/@components";
+import { useNavigate } from "react-router-dom";
+import { LogDetailContainer } from "..";
+import * as S from "./style";
+
+export type AccountLogData = {
+  accountLogId: number;
+  date: string;
+  withdraw?: number;
+  deposit?: number;
+  detail?: string;
+  balance: number;
+};
+
+const AccountLog = () => {
+  const accountInfo = {
+    name: "깨비 미래 적금",
+  };
+  const accountLogList: AccountLogData[] = [
+    { accountLogId: 1, date: "2023-5-14", deposit: 10000, detail: "엄마한테 용돈 받음", balance: 10000 },
+    { accountLogId: 2, date: "2023-5-15", deposit: 1000, detail: "설거지", balance: 11000 },
+  ];
+  const navigate = useNavigate();
+
+  return (
+    <TransactionLogLayout handleClick={() => navigate(-1)}>
+      <>
+        <S.TransactionTitle>{accountInfo.name}</S.TransactionTitle>
+        <S.TransactionSubTitle>
+          <S.TransactionDate>날짜</S.TransactionDate>
+          <S.WithdrawAmount>찾으신 금액</S.WithdrawAmount>
+          <S.DepositAmount>맡기신 금액</S.DepositAmount>
+          <S.TransactionDetail>내역</S.TransactionDetail>
+          <S.Balance>잔액</S.Balance>
+        </S.TransactionSubTitle>
+        <S.TransactionLogsContainer>
+          {accountLogList.map((log) => {
+            return <LogDetailContainer log={log} key={log.accountLogId} />;
+          })}
+        </S.TransactionLogsContainer>
+      </>
+    </TransactionLogLayout>
+  );
+};
+
+export default AccountLog;
