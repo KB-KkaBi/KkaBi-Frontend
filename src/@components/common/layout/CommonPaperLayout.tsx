2918d24072b7e1e5a8e0d79ab03fbada1f77fa18
JaeHyun-Lim-dev <bonny06@naver.com>
2023-05-22 18:12:17 +0900

    feat: common paperlayout에 handleClick이 존재하지 않으면 화살표 버튼 렌더하지 않도록 수정

--- src/@components/common/layout/CommonPaperLayout.tsx
+++ src/@components/common/layout/CommonPaperLayout.tsx
@@ -11,15 +11,17 @@ type Props = {
  * chilren을 종이모양 Background 안에 렌더합니다.
  * @returns
  */
-export default function CommonPaperLayout({ handleClick = () => {}, children }: Props) {
+export default function CommonPaperLayout({ handleClick, children }: Props) {
   // const themeContext = useContext(ThemeContext);
   return (
     <S.CommonBackground color="#5f564c">
-      <S.ButtonContainer onClick={() => handleClick()}>
-        <IconButton width={15} height={15}>
-          <BackArrowIcon width={15} height={15} fillColor="#5f564c" />
-        </IconButton>
-      </S.ButtonContainer>
+      {!!handleClick && (
+        <S.ButtonContainer onClick={() => handleClick()}>
+          <IconButton width={15} height={15}>
+            <BackArrowIcon width={15} height={15} fillColor="#5f564c" />
+          </IconButton>
+        </S.ButtonContainer>
+      )}
       <S.PaperBackground>{children}</S.PaperBackground>
     </S.CommonBackground>
   );
