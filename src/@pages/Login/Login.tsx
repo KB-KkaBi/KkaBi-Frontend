3fc7196a73b8511e346f0a2402584b45b6f7557c
SeungGyu Kwak <62579544+SeungGyu-Kwak@users.noreply.github.com>
2023-05-24 18:05:57 +0900

    feat : 로그인 API 했음 (#50)

--- src/@pages/Login/Login.tsx
+++ src/@pages/Login/Login.tsx
@@ -2,6 +2,8 @@ import { Button, Modal, PaperLayout, TextField } from "@/@components";
 import React, { useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as S from "./styles/loginStyle";
+import { useMutation } from "react-query";
+import { postLogin } from "@/api/login";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -10,6 +12,15 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const [open, setOpen] = useState(false); //Modal Open
   const [isLoginOk, setIsLoginOk] = useState(true); //로그인 가능한지 안한지!
+
+  const { mutate: loginPost } = useMutation(postLogin, {
+    onSuccess: (response) => {
+      navigate("/home");
+    },
+    onError: (error) => {
+      handleModalOpen();
+    },
+  });
 
   const handleModalOpen = useCallback(() => {
     setOpen(true);
@@ -26,38 +37,39 @@ const Login = () => {
     setPassword(e.target.value);
   };
 
-  //login 할때 할 함수
-  const handleLoginClicked = useCallback(async () => {
-    console.debug(email, password, isLoginOk, setIsLoginOk);
-    handleModalOpen();
+  /**
+   * 로그인할 때 할 함수
+   */
+  const handleLoginClicked = () => {
     /**
      * 이메일과 비밀번호를 전송한다.
      * 결과로 status = 200이 오면 홈페이지로 가기
      * 로그인이 안되면 에러 모달 띄어주고 다시 로그인페이지로 가기
      * 로그인이 되면 사용자의 닉네임을 userNickname recoil 변수에 추가하기
      * */
-  }, []);
+    email && password && loginPost({ email: email, pw: password });
+  };
 
   return (
     <PaperLayout
       handleClick={() => {
         navigate(-1);
       }}>
-      <S.LoginFormContainer
-        onSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+      <S.LoginRootContainer
+        onSubmit={(e) => {
           e.preventDefault();
           handleLoginClicked();
         }}>
         <S.EmailInputWrapper>
           <p>EMAIL</p>
-          <TextField placeholder="이메일을 입력해주세요" type="email" onChange={handleEmailChange} />
+          <TextField placeholder="이메일을 입력해주세요" type="text" onChange={handleEmailChange} />
         </S.EmailInputWrapper>
 
         <S.PasswordInputWrapper>
           <p>PASSWORD</p>
           <TextField placeholder="비밀번호를 입력해주세요" type="password" onChange={handlePasswordChange} />
         </S.PasswordInputWrapper>
-        <Button color="primary" type="submit">
+        <Button color="primary" type="submit" onClick={handleLoginClicked}>
           확인
         </Button>
         <Modal open={open} onClose={handleModalClose}>
@@ -66,7 +78,7 @@ const Login = () => {
             <Button onClick={handleModalClose}>확인</Button>
           </S.ModalWrapper>
         </Modal>
-      </S.LoginFormContainer>
+      </S.LoginRootContainer>
     </PaperLayout>
   );
 };
