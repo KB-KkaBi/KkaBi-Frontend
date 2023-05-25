f405901257a2329db9b024c6e072121ab6fb84df
Jaehyun Lim <32118818+JaeHyun-Lim-dev@users.noreply.github.com>
2023-05-25 13:18:22 +0900

    Style#54 quiz/route (#55)
    * style: 투자 페이지 라우팅 통합을 위한 폴더 이동
    
    * style: dummy invest page 제거
    
    * style: 라우팅 변경에 따른 import 루트 및 라우트 주소 수정
    
    * style: 라우팅 통합을 위한 폴더 위치 변경
    
    * fix: 라우팅 룰 변경에 따라 navigate의 parameter 수정
    
    * fix: 라우팅 변경에 따른 navigate 수정

--- src/@pages/index.tsx
+++ src/@pages/index.tsx
@@ -5,5 +5,4 @@ export { Mypage } from "./MyPage";
 export { default as Profile } from "./Register/Profile";
 export { default as Register } from "./Register/Register";
 export { Bank } from "./bank";
-export { default as Quiz } from "./quiz/Quiz";
-export { Invest } from "./treasure";
+export { Invest } from "./invest";
