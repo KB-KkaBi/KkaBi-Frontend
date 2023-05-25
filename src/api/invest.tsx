098372491ad92897fc6b27ec80dd950b3c04fe83
JaeHyun-Lim-dev <bonny06@naver.com>
2023-05-25 14:53:54 +0900

    fix: get api에 treasureId parameter 추가

--- src/api/invest.tsx
+++ src/api/invest.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function getQuizList(treasureId: number) {
-  const data = await axios.get(`${import.meta.env.VITE_BASE_URL}/quiz-info/${treasureId}`);
+  const data = await axios.get(`${import.meta.env.VITE_BASE_URL}/quiz-info?treasureId=${treasureId}`);
   console.debug(data);
   return data?.data;
 }
