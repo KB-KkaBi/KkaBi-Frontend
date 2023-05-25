b29531814dbe049c3e21bf43a141469146980f34
JaeHyun-Lim-dev <bonny06@naver.com>
2023-05-25 15:17:42 +0900

    fix: get 요청에 withCredentials 추가

--- src/api/invest.tsx
+++ src/api/invest.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function getQuizList(treasureId: number) {
-  const data = await axios.get(`${import.meta.env.VITE_BASE_URL}/quiz-info?treasureId=${treasureId}`);
+  const data = await axios.get(`/api/quiz-info?treasureId=${treasureId}`, { withCredentials: true });
   console.debug(data);
   return data?.data;
 }
