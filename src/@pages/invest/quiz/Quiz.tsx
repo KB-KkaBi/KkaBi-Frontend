518e2d36217ea1add65678eeb8ceeaa317e1bea1
Jaehyun Lim <32118818+JaeHyun-Lim-dev@users.noreply.github.com>
2023-05-26 15:26:58 +0900

    Feat#66 invest/post (#67)
    * feat: 내 계좌 가져오는 Get 함수 작성
    
    * fix: proxy 안되는 오류 해결
    
    * feat: 내 계좌 보이기 api 연결함수 구현
    
    * feat: getQuizInfo 추가
    
    * feat: 내가 가진 예금통장만 보이도록 구현
    
    * fix: 임시 테스트용 get url 수정
    
    * feat: 퀴즈 목록 조회 api 추가, 임시로 optional chaining 적용
    
    * fix: get api에 treasureId parameter 추가
    
    * feat: 퀴즈 데이터 get한 후에 버튼 배열 만들도록 수정
    
    * feat: 내가 가진 예금 통장 뷰 구현
    
    * style: 사용하지 않는 type 제거
    
    * feat: 클릭했을 때도 효과 있도록 구현
    
    * etc: 불필요 코드 삭제
    
    * fix: get 요청에 withCredentials 추가
    
    * etc: 시도 중
    
    * feat: 백버튼 추가
    
    * feat: 투자 과정 리코일 추가
    
    * style: 리코일 타입 선언
    
    * feat: 투자 리코일 구현 성공
    
    * etc: 작업중
    
    * feat: 기본 api 추가를 위한 data type 추가, recoil에 기본 atom 추가
    
    * feat: invest - post 관련 api 추가
    
    * feat: 통장에 돈이 충분하지 않은 경우 reject되도록 구현
    
    * feat: 보물 선택/투자 갯수 선택 화면에서 setRecoilState 추가
    
    * feat: 통장의 돈이 모자라지 않은 경우에만 넘어가도록 구현
    
    * fix: accountMoney 안 넘어오는 문제 수정
    
    * [ feat ] 계좌 로그 포스트 성공
    
    * style: autolint
    
    * feat: api 호출 수정
    
    * feat: quiz post연동
    
    * fix: 투자 로직에 재진입 시 기존 invest 데이터 초기화 하도록 코드 추가
    
    * feat: develop merge에 따라 네이밍 수정
    
    * fix: reset recoil state 제거
    
    * fix: setInvestData 수정
    
    * fix: post 요청 시 정답 부분 반영되도록 수정
    
    ---------
    
    Co-authored-by: Seojisoo20191941 <amy0480@naver.com>
    Co-authored-by: Awarduuu <award_uuu@naver.com>

--- src/@pages/invest/quiz/Quiz.tsx
+++ src/@pages/invest/quiz/Quiz.tsx
@@ -1,27 +1,50 @@
 import { Button, Modal, PaperLayout } from "@/@components";
-import { getQuizList } from "@/api/invest";
-import { investInfo } from "@/recoil/Invest";
+import { getQuizList, postQuizAnswer } from "@/api/invest";
+import { InvestDTO, investInfo } from "@/recoil/Invest";
 import { selectedButtonArray, selectedButtonIndex } from "@/recoil/Quiz";
 import hangul from "hangul-js";
-import { useCallback, useContext, useMemo, useState } from "react";
-import { useQuery } from "react-query";
+import { useCallback, useContext, useEffect, useState } from "react";
+import { useMutation, useQuery } from "react-query";
 import { useNavigate } from "react-router-dom";
 import { useRecoilState, useRecoilValue, useResetRecoilState } from "recoil";
 import { ThemeContext } from "styled-components";
 import MemoizedAnswerSelector from "./AnswerSelector";
 import * as S from "./style";
 
-function shuffle(array: string[]) {
+function shuffle(array: string[]): string[] {
   return array?.sort(() => Math.random() - 0.5);
 }
 
-const treasureId: number = 2;
-
 const Quiz = () => {
-  const { data: quizData } = useQuery(["quizInfo", treasureId], () => getQuizList(treasureId));
-  const randomQuiz = Array.isArray(quizData) ? quizData[Math.floor(Math.random() * quizData.length)] : "";
+  const [investData, setInvestData] = useRecoilState(investInfo);
+  const [quizData, setQuizData] = useState({ problem: "" }); // 화면에 표시할 퀴즈의 정보
+  const [buttonArray, setButtonArray] = useState<string[]>();
+  const [success, setSuccess] = useState(); // 답 제출 시 정답 여부
+  const [total, setTotal] = useState(); // 답 제출 시 결과로 만들어진 보물 개수
+  const { data: quizArray } = useQuery(["quizInfo", investData.treasureId], () => getQuizList(investData.treasureId));
+
+  useEffect(() => {
+    if (Array.isArray(quizArray)) {
+      const randomQuiz = Array.isArray(quizArray) ? quizArray[Math.floor(Math.random() * quizArray.length)] : undefined;
+      if (randomQuiz) {
+        setQuizData(randomQuiz);
+        setInvestData(({ quizId, ...data }) => {
+          const newData = {
+            ...data,
+            quizId: randomQuiz.quizId || 0,
+          };
+          console.debug("quiz - ", newData);
+          return newData;
+        });
+        if (typeof randomQuiz.array === "string") {
+          const arr = shuffle(randomQuiz.array.split(", "));
+          setButtonArray(arr);
+        }
+      }
+    }
+  }, [quizArray]);
+
   const selectedArray = useRecoilValue(selectedButtonArray);
-  const buttonArray = useMemo(() => shuffle(randomQuiz?.array?.split(", ")), [randomQuiz]);
   const navigate = useNavigate();
   const [open, setOpen] = useState<boolean>(false);
   const handleOpen = () => setOpen(true);
@@ -33,17 +56,36 @@ const Quiz = () => {
 
   const resetButtonArray = useResetRecoilState(selectedButtonArray);
   const resetIndexArray = useResetRecoilState(selectedButtonIndex);
-  const [investData, setInvestData] = useRecoilState(investInfo);
+  // const [investData, setInvestData] = useRecoilState(investInfo);
 
   const handleReset = useCallback(() => {
     resetButtonArray();
     resetIndexArray();
   }, []);
+  const { mutate: investPost } = useMutation(
+    (investData: InvestDTO) => {
+      const { accountId, ...investPostData } = investData;
+      return postQuizAnswer(investPostData);
+    },
+    {
+      onSuccess: (response) => {
+        console.debug(response);
+        setSuccess(response.data?.success);
+        setTotal(response.data?.total);
+        handleOpen();
+        handleReset();
+      },
+    },
+  );
 
   const handleSubmit = () => {
-    console.debug("제출: ", hangul.assemble(selectedArray));
-    handleOpen();
-    handleReset();
+    const ans = hangul.assemble(selectedArray);
+    setInvestData((data) => {
+      const newData = { ...data, answer: ans };
+      console.debug(newData);
+      return newData;
+    });
+    investPost(investData);
   };
 
   return (
@@ -51,8 +93,8 @@ const Quiz = () => {
       <S.QuizWrapper>
         <S.MyAnswer>나의 답: {hangul.assemble(selectedArray)}</S.MyAnswer>
         <S.QuizMainContainer>
-          <S.QuizContent>{randomQuiz.problem}</S.QuizContent>
-          <MemoizedAnswerSelector array={buttonArray} />
+          <S.QuizContent>{quizData?.problem || ""}</S.QuizContent>
+          <MemoizedAnswerSelector array={buttonArray || []} />
         </S.QuizMainContainer>
         <S.ButtonContanier>
           <Button onClick={handleReset} disabled={!selectedArray.length}>
@@ -65,11 +107,12 @@ const Quiz = () => {
       </S.QuizWrapper>
       <Modal open={open}>
         <S.ModalContent>
-          <div>정답입니다</div>
+          <div>{success ? "정답" : "오답"}입니다</div>
           <div>
-            투자한 <S.TreasureCount $color={themeContext?.colors.darkYellow}>{500}</S.TreasureCount>개의 반지가
+            투자한 <S.TreasureCount $color={themeContext?.colors.darkYellow}>{investData.count}</S.TreasureCount>
+            개의 반지가
             <br />
-            <S.TreasureCount $color={"#FC1616"}>{525}</S.TreasureCount>개가 되었습니다
+            <S.TreasureCount $color={success ? "#FC1616" : "#162DFC"}>{total}</S.TreasureCount>개가 되었습니다
           </div>
           <Button onClick={handleClose}>확인</Button>
         </S.ModalContent>
