4a581945443b389a94ef501a2b963dd3200c8735
JaeHyun-Lim-FE <bonny06@naver.com>
2023-05-26 11:17:00 +0900

    feat: api 호출 수정

--- src/api/invest.tsx
+++ src/api/invest.tsx
@@ -1,11 +1,11 @@
 import { InvestDataTypes } from "@/core/investData";
 import axios from "axios";
 
-export async function getQuizList(treasureId: number) {
+export const getQuizList = async (treasureId: number) => {
   const data = await axios.get(`/api/quiz-info?treasureId=${treasureId}`, { withCredentials: true });
-  console.debug(data);
-  return data?.data;
-}
+
+  return data.data;
+};
 
 export async function postQuizAnswer(investData: InvestDataTypes) {
   const data = await axios.post(`/api/invest`, investData, { withCredentials: true });
