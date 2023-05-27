a1b6050d1fa4ce0bc83e2bacb1060a689437353f
seojisoosoo <76681519+seojisoosoo@users.noreply.github.com>
2023-05-27 21:30:03 +0900

    Feat#81 account/get account log (#85)
    * fix
    
    * feat: 계좌 로그 api구현
    
    * design: 페이지네이션 디자인 구현
    
    * feat: 페이지 네이션 성공
    
    * feat: 로그 관련 api 구현
    
    * feat: 로그 props 수정
    
    * feat: 페이지네이션 데이터 가져오기 성공
    
    * feat: 출금의 경우 절댓값으로 수정
    
    * etc: 불필요 코드 삭제
    
    * feat: id로 수정
    
    ---------
    
    Co-authored-by: SangWoo Yun <64531982+Awarduuu@users.noreply.github.com>

--- src/recoil/bank.tsx
+++ src/recoil/bank.tsx
@@ -1,12 +1,14 @@
 import { atom } from "recoil";
 
-export const bankLog=atom({
-    key:"bankLog",
-    default:{
-        accountId: -1,
-        accountLogMoney: 0,
-        transactionAmount: 0,
-        transactionReason: ``,
-        transactionType: ``,
-      }
-})
+
+export const bankLog = atom({
+  key: "bankLog",
+  default: {
+    accountId: -1,
+    accountLogMoney: 0,
+    transactionAmount: 0,
+    transactionReason: ``,
+    transactionType: ``,
+  },
+});
+
