4a96a96db763241a634d07a008eb4012cee89f65
SeungGyu Kwak <62579544+SeungGyu-Kwak@users.noreply.github.com>
2023-05-28 05:58:15 +0900

    Hotfix#96 common/homefix (#97)
    * fix : 회원가입시 캐릭터, 닉네임부분 수정
    
    * feat : 랜딩페이지 버튼 달았음

--- src/@pages/Landing.tsx
+++ src/@pages/Landing.tsx
@@ -1,15 +1,48 @@
 import { Button } from "@/@components";
 import LandingLayout from "@/@components/Landing/LandingLayout";
 import { LandingLogoIc } from "@/assets";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { styled } from "styled-components";
 
+import landingPlay from "@/assets/image/landingBgmPlay.png";
+import landingStop from "@/assets/image/landingBgmStop.png";
+import developer from "@/assets/image/developer.png";
+import guide from "@/assets/image/guide.png";
+
+import landingMusic from "@/assets/music/landingmypage2.mp3";
+import { BtnWrapper } from "./MyPage/styles/mypageStyle";
+
 const Landing = () => {
   let navigate = useNavigate();
+  const audioRef = useRef<any>(null);
+  const [play, setPlay] = useState<boolean>(false);
 
+  const playClick = () => {
+    audioRef.current.play();
+  };
+
+  const stopClick = () => {
+    audioRef.current.pause();
+  };
   return (
     <LandingLayout>
+      <ButtonController>
+        <ButtonWrapper onClick={() => {}}>
+          <img src={guide} alt="guide" />
+        </ButtonWrapper>
+        <ButtonWrapper onClick={() => {}}>
+          <img src={developer} alt="developer" />
+        </ButtonWrapper>
+        <ButtonWrapper onClick={stopClick}>
+          <img src={landingStop} alt="landingStop" />
+        </ButtonWrapper>
+        <ButtonWrapper onClick={playClick}>
+          <img src={landingPlay} alt="landingPlay" />
+        </ButtonWrapper>
+      </ButtonController>
       <LandingWrapper>
+        <audio src={landingMusic} ref={audioRef} autoPlay />
         <LandingLogoIcon />
         <ButtonContainer>
           <Button onClick={() => navigate("/register")}>회원가입</Button>
@@ -22,6 +55,15 @@ const Landing = () => {
 
 export default Landing;
 
+const ButtonController = styled.div`
+  position: absolute;
+  left: 140rem;
+  top: 2rem;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  column-gap: 1rem;
+  row-gap: 1rem;
+`;
 const LandingWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -32,6 +74,10 @@ const LandingLogoIcon = styled(LandingLogoIc)`
   width: 150rem;
   margin-bottom: 6rem;
   margin-right: 5rem;
+`;
+
+const ButtonWrapper = styled.div`
+  cursor: pointer;
 `;
 const ButtonContainer = styled.div`
   display: flex;
