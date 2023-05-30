18a85decce8dc438cf990c52fd0bdc7601e122b5
Seojisoo20191941 <amy0480@naver.com>
2023-05-30 10:41:25 +0900

    fix:프록시 에러 수정 중

--- src/api/login.tsx
+++ src/api/login.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function postLogin(userData: any) {
-  const data = await axios.post(`${import.meta.env.VITE_APP_BASE_URL}/login`, userData, { withCredentials: true });
+  const data = await axios.post(`/api/login`, userData, { withCredentials: true });
 
   console.log(data);
   return data;
