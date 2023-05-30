ac4aa4eef1658fe589222a1a55e425f2665dc1e2
JaeHyun-Lim-dev <bonny06@naver.com>
2023-05-30 11:08:59 +0900

    chore: 빌드 에러 수정중(vite config)

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,35 @@
 import react from "@vitejs/plugin-react";
-import { env } from "process";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    react(),
-    svgr({
-      svgrOptions: {
-        // svgr options
-      },
-    }),
-  ],
-  server: {
-    proxy: {
-      "/api": {
-        target: env.VITE_APP_BASE_URL,
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      tsconfigPaths(),
+      react(),
+      svgr({
+        svgrOptions: {
+          // svgr options
+        },
+      }),
+    ],
+    define: {
+      __APP_ENV__: env.APP_ENV,
+    },
+    server: {
+      proxy: {
+        "/api": {
+          // target: env.VITE_APP_BASE_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
-  build: { chunkSizeWarningLimit: 7000 },
+    build: { chunkSizeWarningLimit: 7000 },
+  };
 });
