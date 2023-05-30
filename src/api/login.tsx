4828040425665eba70d73c18be8f41828b329a3b
JaeHyun-Lim-dev <bonny06@naver.com>
2023-05-30 10:52:33 +0900

    chore: 빌드 api 오류 수정 중

--- src/api/login.tsx
+++ src/api/login.tsx
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export async function postLogin(userData: any) {
-  const data = await axios.post(`/api/login`, userData, { withCredentials: true });
+  const data = await axios.post(import.meta.env.VITE_APP_BASE_URL + `/api/login`, userData, {
+    withCredentials: true,
+  });
 
   console.log(data);
   return data;
