55f7474a15ba511bc164e1b16f461b06b8e1cae6
JaeHyun-Lim-dev <bonny06@naver.com>
2023-05-30 11:28:35 +0900

    chore: 빌드용 미사용변수 제거

--- src/api/login.tsx
+++ src/api/login.tsx
@@ -1,13 +1,13 @@
 import axios from "axios";
-const config = {
-  headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
-    "Access-Control-Allow-Origin": "*",
-  },
-  data: null,
-  withCredentials: true,
-};
+// const config = {
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//     "Access-Control-Allow-Origin": "*",
+//   },
+//   data: null,
+//   withCredentials: true,
+// };
 export async function postLogin(userData: any) {
   const data = await axios.post(`https://kkabi.shop:9000/login`, userData, {
     withCredentials: true,
